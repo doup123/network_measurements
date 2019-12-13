bf32252b39b7774d13b30b54d37d8ff16d709b97
marinos <mdimolianis@netmode>
2019-12-13 12:30:32 +0100

    Fixed files for results to be per monitored instance

--- twping_measurement.py
+++ twping_measurement.py
@@ -8,7 +8,7 @@ else:
     #dst_ip="10.1.1.100"
     src_ip = sys.argv[1]
     dst_ip = sys.argv[2]
-    twping_results_file = "./twping_results"
+    twping_results_file = "./twping_results_"+str(src_ip)+"_"+str(dst_ip)
     os.system("twping "+str(dst_ip)+">"+str(twping_results_file))
     twping_service=parse_twping(twping_results_file)
     influxdb_preprocess_write(twping_service,src_ip,dst_ip)
