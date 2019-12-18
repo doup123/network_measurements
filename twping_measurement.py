581737de53d9a5048aba74ecb69e003977f0d3d6
marinos <mdimolianis@netmode>
2019-12-18 11:47:05 +0100

    Reducing the number of test packets from 100 to 10 for scalability reasons in GTS testbed

--- twping_measurement.py
+++ twping_measurement.py
@@ -9,6 +9,6 @@ else:
     src_ip = sys.argv[1]
     dst_ip = sys.argv[2]
     twping_results_file = "./twping_results_"+str(src_ip)+"_"+str(dst_ip)
-    os.system("twping "+str(dst_ip)+">"+str(twping_results_file))
+    os.system("twping "+str(dst_ip)+" -c 10 >"+str(twping_results_file))
     twping_service=parse_twping(twping_results_file)
     influxdb_preprocess_write(twping_service,src_ip,dst_ip)
