f176ec7ef364eb2e10267e354ad4a5439e335929
mdimolianis@netmode.ntua.gr <mdimolianis@netmode.ntua.gr>
2020-07-13 13:51:58 +0200

    Fixed parsing for values greater than 100 since twping is not returning decimal-placed values

--- twping_parser.py
+++ twping_parser.py
@@ -9,12 +9,12 @@ def parse_twping(twping_results_file):
     sid = re.compile('SID:\t(?P<sid>.*)')
     first = re.compile('first:\t(?P<first>.*)')
     last = re.compile('last:\t(?P<time>.*)')
-    rtt = re.compile('round-trip time min/median/max = (?P<rtt_min>\d{1,10}\.\d{1,4})/(?P<rtt_median>\d{1,10}\.\d{1,4})/(?P<rtt_max>\d{1,10}\.\d{1,4}).*')
-    send_time = re.compile('send time min/median/max = (?P<send_time_min>\d{1,10}\.\d{1,4})/(?P<send_time_median>\d{1,10}\.\d{1,4})/(?P<send_time_max>\d{1,10}\.\d{1,4}).*')
-    reflect_time = re.compile('reflect time min/median/max = (?P<reflect_time_min>\d{1,10}\.\d{1,4})/(?P<reflect_time_median>\d{1,10}\.\d{1,4})/(?P<reflect_time_max>\d{1,10}\.\d{1,4}).*')
-    two_way_jitter = re.compile('two-way jitter = (?P<two_way_jitter>\d{1,10}\.\d{1,4}).*')
-    send_jitter = re.compile('send jitter = (?P<send_jitter>\d{1,10}\.\d{1,4}).*')
-    reflect_jitter = re.compile('reflect jitter = (?P<reflect_jitter>\d{1,10}\.\d{1,4}).*')
+    rtt = re.compile('round-trip time min/median/max = (?P<rtt_min>\d{1,10}(\.\d{1,4})?)/(?P<rtt_median>\d{1,10}(\.\d{1,4})?)/(?P<rtt_max>\d{1,10}(\.\d{1,4})?).*')
+    send_time = re.compile('send time min/median/max = (?P<send_time_min>\d{1,10}(\.\d{1,4})?)/(?P<send_time_median>\d{1,10}(\.\d{1,4})?)/(?P<send_time_max>\d{1,10}(\.\d{1,4})?).*')
+    reflect_time = re.compile('reflect time min/median/max = (?P<reflect_time_min>\d{1,10}(\.\d{1,4})?)/(?P<reflect_time_median>\d{1,10}(\.\d{1,4})?)/(?P<reflect_time_max>\d{1,10}(\.\d{1,4})?).*')
+    two_way_jitter = re.compile('two-way jitter = (?P<two_way_jitter>\d{1,10}(\.\d{1,4})?).*')
+    send_jitter = re.compile('send jitter = (?P<send_jitter>\d{1,10}(\.\d{1,4})?).*')
+    reflect_jitter = re.compile('reflect jitter = (?P<reflect_jitter>\d{1,10}(\.\d{1,4})?).*')
 
     regex_list=[sid,first,last,rtt,send_time,reflect_time,two_way_jitter,send_jitter,reflect_jitter]
     with open(twping_results_file) as fp:
